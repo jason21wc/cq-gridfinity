b0504186943ca2ca1b3dcc087c0bc4c262497038
Jason Collier <jason.collier@me.com>
2026-02-26 20:04:40 -0700

    Revert "Add modular drawer system: sliding drawer body and chest frame"
    This reverts commit afb8c3b504fd5695b1bc6fe10feb51a4e09167a3.

--- cqgridfinity/__init__.py
+++ cqgridfinity/__init__.py
@@ -18,5 +18,4 @@ from .gf_box import GridfinityBox, GridfinitySolidBox
 from .gf_drawer import GridfinityDrawerSpacer
 from .gf_ruggedbox import GridfinityRuggedBox
 from .extended import GridfinityLid, GridfinityItemHolder, GridfinityTray
-from .extended import GridfinityDrawer, GridfinityDrawerChest
 from .extended import CutoutProfile, RoundCutout, RectCutout
